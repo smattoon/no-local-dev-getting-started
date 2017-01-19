d60e7b678f34c87e599873b8aaae39236f92afe8
Scott Mattoon <smattoon@pacbell.net>
2017-01-18 17:19:32 -0800

    Update app.rb

--- app.rb
+++ app.rb
@@ -9,12 +9,8 @@ class Hive < ActiveRecord::Base
 end
 
 get "/hives" do
-  dashboard_url = 'https://dashboard.heroku.com/'
-  match = /(.*?)\.herokuapp\.com/.match(request.host)
-  dashboard_url << "apps/#{match[1]}/resources" if match && match[1]
-  redirect to(dashboard_url)
 
-#  @hives = Hive.all
+  @hives = Hive.all
 #  erb :index
 end
 
