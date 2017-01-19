23367e82725c47fa7546451359676a4f485af971
Scott Mattoon <smattoon@pacbell.net>
2017-01-18 18:23:30 -0800

    Update app.rb

--- app.rb
+++ app.rb
@@ -29,6 +29,15 @@ get "/contacts" do
   erb :index
 end
 
+class Account < ActiveRecord::Base
+  self.table_name = 'salesforce.account'
+end
+
+get "/accounts" do
+  @accounts = Account.all
+  erb :index
+end
+
 get "/create" do
   dashboard_url = 'https://dashboard.heroku.com/'
   match = /(.*?)\.herokuapp\.com/.match(request.host)
